7dd0a87826a77b7d3075680378a52d76f1cce971
Hannahadora <hejimofor141@uniport.edu.ng>
2021-03-20 22:54:21 +0100

    done

--- main.js
+++ main.js
@@ -1,50 +1,73 @@
 const counterOutput = document.querySelector('.counter');
 const minus = document.querySelector('.minus');
-const up = document.querySelector('.add');
+const add = document.querySelector('.add');
 const reset = document.querySelector('.reset');
 const step = document.querySelector('#step');
 
-let counter = 0;
+let count = 0;
 
 function adjustCounter(){
     adjustColor();
-    counterOutput.innerText = counter;
+    counterOutput.innerText = count;
 }
 
-function adjustColor() {
-	if (counter === 0) {
-		counterOutput.style.color = 'white';
-	} else if (counter < 0) {
-		counterOutput.style.color = 'red';
-	} else if (counter > 0) {
-		counterOutput.style.color = 'blue';
-	};
-}
+// function adjustColor() {
+// 	if (counter === 0) {
+// 		counterOutput.style.color = 'white';
+// 	} else if (counter < 0) {
+// 		counterOutput.style.color = 'red';
+// 	} else if (counter > 0) {
+// 		counterOutput.style.color = 'blue';
+// 	};
+// }
 
-up.addEventListener('click', clickUp())
-    e.preventDefault(e);
+// add.addEventListener('click', clickUp())
+//     e.preventDefault(e);
 
-function clickUp(e) {
-	counter = total + parseInt(step.value);
-	adjustCounter();
-}
-
-
-minus.addEventListener('click', clickDown())
-    e.preventDefault(e);
-
-function clickDown(e) {
-	counter = total - parseInt(step.value);
-	adjustCounter();
-}
+// function clickUp(e) {
+// 	counter = total + parseInt(step.value);
+// 	adjustCounter();
+// }
 
 
-reset.addEventListener('click', resetCounter())
-    e.preventDefault(e);
+// minus.addEventListener('click', clickDown())
+//     e.preventDefault(e);
 
-function resetCounter() {
-	counter = 0;
-	adjustCounter();
-}
+// function clickDown(e) {
+// 	counter = total - parseInt(step.value);
+// 	adjustCounter();
+// }
+
+
+// reset.addEventListener('click', resetCounter())
+//     e.preventDefault(e);
+
+// function resetCounter()  {
+// 	counter = 0;
+// 	adjustCounter();
+// }
+
+// adjustCounter();
+
 
 adjustCounter();
+
+add.addEventListener("click",()=>{
+    count++;
+    adjustCounter();
+}) ;
+
+minus.addEventListener("click",()=>{
+    count--;
+    adjustCounter();
+});
+
+// function adjustColor(){
+// 	if (count === 0) {
+// 		counterOutput.style.color = 'white';
+// 	} else if (counter < 0) {
+// 		counterOutput.style.color = 'red';
+// 	} else if (counter > 0) {
+// 		counterOutput.style.color = 'blue';
+// 	};
+// };
